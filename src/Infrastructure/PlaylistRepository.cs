33f660c756436047adde6bb95c9360340730a25b
Sailesh1993 <karki.saileshnep@gmail.com>
2023-06-25 19:33:34 +0300

    added PlaylistRepository class in Infrastructure layer

---
+++ src/Infrastructure/PlaylistRepository.cs
@@ -0,0 +1,65 @@
+namespace MediaPlayer.src.Core.Infrastructure
+{
+    public class PlaylistRepository
+    {
+        private readonly Dictionary<string,List<string>> _playlists;
+
+        public PlaylistRepository()
+        {
+            _playlists = new Dictionary<string, List<string>>();
+        }
+        
+        public bool CreatePlaylist(string playlistName)
+        {
+            if(string.IsNullOrEmpty(playlistName) || _playlists.ContainsKey(playlistName))
+            {
+                return false;
+            }
+            _playlists.Add(playlistName, new List<string>());
+            return true;
+        }
+        
+        public bool AddSongToPlayList(string playlistName, string songFilePath)
+        {
+            if(string.IsNullOrEmpty(playlistName) || string.IsNullOrEmpty(songFilePath) || !_playlists.ContainsKey(playlistName))
+            {
+                return false;
+            }
+            List<string> playlist = _playlists[playlistName];
+            if(playlist.Contains(songFilePath))
+            {
+                return false;
+            }
+            playlist.Add(songFilePath);
+            return true;
+        }
+        public bool RemoveSongFromPlaylist(string playlistName, string songFilePath)
+        {
+            if(string.IsNullOrEmpty(playlistName) || string.IsNullOrEmpty(songFilePath) || !_playlists.ContainsKey(playlistName))
+            {
+                return false;
+            }
+            List<string> playlist = _playlists[playlistName];
+            return playlist.Remove(songFilePath);
+        }
+
+        public List<string> GetPlaylistSongs(string playlistName)
+        {
+            if(string.IsNullOrEmpty(playlistName) || !_playlists.ContainsKey(playlistName))
+            {
+                return new List<string>();
+            }
+            return new List<string>(_playlists[playlistName]);
+        }
+        
+        public List<string> GetAllPlaylists()
+        {
+            if(_playlists.Keys.Count !> 0)
+            {
+                Console.WriteLine("No playlist, create one");
+                return new List<string>();
+            }
+            return _playlists.Keys.ToList();
+        }
+    }
+}
